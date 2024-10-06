347fec3a82e84d411724c6e5730892f77e25b7e1
Anna <annakovalchuk2008@gmail.com>
2024-10-06 13:48:58 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+function checkForSpam(message) {
+    const word1 = "spam";
+    const word2 = "sale";
+   if (message.toLowerCase().includes(word1)){
+    return true
+   } else if (message.toLowerCase().includes(word2)) {
+    return true;
+   } else {
+    return false;
+   }
+}
+
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
